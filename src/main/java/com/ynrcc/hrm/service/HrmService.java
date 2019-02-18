d10b02ae9c8957f3ba1ce3d83934ca87590e9faf
LycPandaria <lyc0873@gmail.com>
2019-02-18 17:22:42 +0800

    增加 服务层 小部分代码，更改pom配置，增加分页标签类

---
+++ src/main/java/com/ynrcc/hrm/service/HrmService.java
@@ -0,0 +1,46 @@
+package com.ynrcc.hrm.service;
+
+import com.ynrcc.hrm.dao.*;
+import com.ynrcc.hrm.model.*;
+import com.ynrcc.hrm.util.tag.PageModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 外包人员管理服务层
+ */
+// Propagation.REQUIRED 属性指有事务就处于当前事务中，没有事务就创建一个事务
+// Isolation.DEFAULT 属性表示使用事务数据库的默认隔离级别
+@Transactional(propagation = Propagation.REQUIRED,isolation = Isolation.DEFAULT)
+@Service
+public class HrmService {
+
+    // 自动注入 Dao 层对象
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private DeptDao deptDao;
+    @Autowired
+    private EmployeeDao employeeDao;
+    @Autowired
+    private JobDao jobDao;
+    @Autowired
+    private NoticeDao noticeDao;
+    @Autowired
+    private DocumentDao documentDao;
+
+    /**
+     * 登陆
+     * @param loginname
+     * @param password
+     * @return 对应的用户
+     */
+    public User login(String loginname, String password){
+        System.out.println("HrmService login -- >>");
+        return userDao.getByLoginnameAndPassword(loginname,password);
+    }
+
+}
---
+++ src/main/java/com/ynrcc/hrm/util/common/Constants.java
@@ -0,0 +1,7 @@
+package com.ynrcc.hrm.util.common;
+
+public class Constants {
+
+    // 每页默认数据条数
+    public static int PAGE_DEFAULT_SIZE = 4;
+}
